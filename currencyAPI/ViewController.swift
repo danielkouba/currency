5b6406ccb22c3468d93d40ce9af0793d084bbeee
Daniel Kouba <koubaphotog@gmail.com>
2017-02-24 17:24:32 -0800

    Initial Commit

---
+++ currencyAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currencyAPI
+//
+//  Created by Daniel Kouba on 2/24/17.
+//  Copyright © 2017 Daniel Kouba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
